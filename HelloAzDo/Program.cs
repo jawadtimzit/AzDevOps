51679e886c3c45545138dc692b0ca232ec9fd23a
jawadtimzit <56841959+jawadtimzit@users.noreply.github.com>
2025-11-12 10:26:24 -0800

    Delete HelloAzDo directory

--- HelloAzDo/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => Results.Text("Welcome to Azure DevOps Pipelines!", "text/plain"));
-
-app.Run();
